16a7061bbb5b7ae3e2c83efa402e360009f9c2ff
ArchieSachin <archieshah8767@gmail.com>
2025-02-19 17:35:00 +0530

    splash screen added

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,11 +1,26 @@
+import { Link, useNavigation } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import LottieView from 'lottie-react-native';
+import { useRouter } from 'expo-router';
+import { useLayoutEffect } from 'react';
+
 
 export default function App() {
+  const router = useRouter();
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LottieView
+        source={require("../assets/splash2.json")}
+        style={styles.animation}
+        autoPlay
+        loop={false}
+        onAnimationFinish={() => {
+          router.push("/sign-in");  // Check if the user is logged in after animation finishes
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +28,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+  },
+  animation: {
+    width: "100%",
+    height: "100%",
   },
 });
